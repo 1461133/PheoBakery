b952cbb09f70decb0cc6d742838fc9bf5ec5fd2c
TUANNGUYEN\Tuan Nguyen <upinguyen96@gmail.com>
2017-06-11 23:50:26 +0700

    update csdl, đăng ký khách hàng

--- PheoBakery/Controllers/KHACHHANGsController.cs
+++ PheoBakery/Controllers/KHACHHANGsController.cs
@@ -37,7 +37,10 @@ namespace PheoBakery.Controllers
         }
 
         // GET: KHACHHANGs/Create
-        
+        public ActionResult Create()
+        {
+            return View();
+        }
 
         // POST: KHACHHANGs/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
